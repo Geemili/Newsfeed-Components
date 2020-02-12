7500a1893fb1bbeca59dd36898ed353e2ba5b590
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-02-12 16:24:39 -0700

    Generate article elements from data

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,72 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+// Convienent API for building HTML stuff
+class El {
+    constructor(elementName) {
+        this.element = document.createElement(elementName);
+    }
+
+    className(className) {
+        this.element.classList.add(className);
+        return this;
+    }
+
+    children(childrenEls) {
+        childrenEls.forEach(el => this.element.appendChild(el.element));
+        return this;
+    }
+
+    text(textContent) {
+        this.element.textContent = textContent;
+        return this;
+    }
+
+    done() {
+        return this.element;
+    }
+}
+
+function p(text) {
+    return new El("p").text(text);
+}
+
+function h2(text) {
+    return new El("h2").text(text);
+}
+
+function span(text) {
+    return new El("span").text(text);
+}
+
+function div(className, children) {
+    const el = new El("div");
+    if (className !== undefined) {
+        el.className(className);
+    }
+    if (children !== undefined) {
+        el.children(children);
+    }
+    return el;
+}
+
+// Create article
+function createArticle(props) {
+     const el = div("article");
+     el.children([
+        h2(props.title),
+        p(props.date).className("date"),
+
+        p(props.firstParagraph),
+        p(props.secondParagraph),
+        p(props.thirdParagraph),
+
+        span().className("expandButton"),
+    ]);
+    return el.done();
+}
+
+const articles = document.querySelector(".articles");
+data.map(createArticle).forEach(el => articles.appendChild(el));
+
